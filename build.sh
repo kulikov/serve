a9666a62b21ff76e894c0e4d101fb20f89f1c57c
kulikov <kulikov.dm@gmail.com>
2016-02-14 03:15:21 +0300

    impr build

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 pushd $DIR
 
 for os in linux darwin; do
-  GOOS=$os CGO_ENABLED=0 go build -v -o $DIR/dist/serve-$os
+  GOOS=$os CGO_ENABLED=0 go build -v -o dist/serve-$os apps/serve.go
 done
 
 popd
