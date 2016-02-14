8c79ff3b59fc986224465dabc301fe28cab48bf9
kulikov <kulikov.dm@gmail.com>
2016-02-15 00:21:42 +0300

    = create pipeline in Go

--- gocd/deploy.go
+++ gocd/deploy.go
@@ -8,6 +8,7 @@ import (
 	"gopkg.in/yaml.v2"
 
 	"../manifest"
+	"net/http"
 )
 
 type (
@@ -83,8 +84,10 @@ func (ea DeployPlugin) Run(conf *viper.Viper, mft *manifest.Manifest) error {
 	if depl.Deploy.Debian != nil {
 		deb := depl.Deploy.Debian
 		project := "inn-" + mft.Info.Name
+		qaNodes := deb.Cluster["qa"]
+		liveNodes := deb.Cluster["live"]
 
-		if qaNodes, ok := deb.Cluster["qa"]; ok {
+		if qaNodes != "" {
 			pipeline.Stages = append(pipeline.Stages, Stage{
 				Name: "Deployment-QA",
 				Jobs: []Job{
@@ -109,14 +112,85 @@ func (ea DeployPlugin) Run(conf *viper.Viper, mft *manifest.Manifest) error {
 						},
 					},
 				},
+			},
+			Stage{
+				Name: "Ready-to-Ship",
+				Approval: Approval{
+					Type: "manual",
+				},
+				Jobs: []Job{
+					Job{
+						Name:      "Approve-Package",
+						Resources: []string{"Builder", "Debian"},
+						Tasks: []Task{
+							Task{
+								Type: "exec",
+								Attributes: TaskAttributes{
+									RunIf:            []string{"passed"},
+									WorkingDirectory: "sources",
+									Command:          "/bin/bash",
+									Arguments: []string{
+										"-c",
+										"/var/go/inn-ci-tools/go/go-package-approve.sh --suffix=-master --patch-version=$GO_PIPELINE_LABEL " +
+											"--project=" + project + " --version=" + mft.Info.Version + " --src-repo=unstable --dst-repo=stable",
+									},
+								},
+							},
+						},
+					},
+				},
+			})
+		}
+
+		if liveNodes != "" {
+			pipeline.Stages = append(pipeline.Stages, Stage{
+				Name: "Deployment-Live",
+				Approval: Approval{
+					Type: "manual",
+					Authorization: Authorization{
+						Roles: []string{"QA"},
+					},
+				},
+				Jobs: []Job{
+					Job{
+						Name:      "Deploy",
+						Resources: []string{"Builder", "Debian"},
+						Tasks: []Task{
+							Task{
+								Type: "exec",
+								Attributes: TaskAttributes{
+									RunIf:            []string{"passed"},
+									WorkingDirectory: "sources",
+									Command:          "/bin/bash",
+									Arguments: []string{
+										"-c",
+										"dig +short " + liveNodes + " | sort | uniq | parallel -j 10 " +
+											"/var/go/inn-ci-tools/go/go-package-deploy.sh --target={} --project=" + project +
+											" --version=" + mft.Info.Version + ".$GO_PIPELINE_LABEL --purge-pattern=" + project + "-v.*",
+									},
+								},
+							},
+						},
+					},
+				},
 			})
 		}
 	}
 
 	bytes, err := json.Marshal(pipeline)
-
 	if err != nil {
 		return err
+	}
+
+	req, _ := http.NewRequest("POST", "https://go.inn.ru/go/api/admin/pipelines", bytes)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Accept", "application/vnd.go.cd.v1+json")
+	req.SetBasicAuth("login", "pass")
+
+	resp, err := http.DefaultClient.Do(req)
+
+	if (resp.StatusCode > 201) {
+		return resp.Status
 	}
 
 	log.Println(string(bytes))
