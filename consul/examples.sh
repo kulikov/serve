bf11ab964fe25ff14c65e790fd8df5a297a862a2
kulikov <kulikov.dm@gmail.com>
2016-04-24 13:08:06 +0300

    = serve examples

--- consul/examples.sh
+++ consul/examples.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# для serve на Go машинках нужен будет /etc/serve/config.yml, 
+# в котором будут адреса марафона, реестра пакетов и прочие конфиги
+
+# 1. запускаем новую версию, дожидаемся появления в консуле
+# 2. находим предыдущую версию и убираем ее сначала из консула, потом из марафона
+# todo: надо подумать как находить полную версию только по build-number
+# --branch 'master' — опционально поле, 'master' по-умолчанию
+serve deploy --env 'qa' --branch 'master' --build-number '34'
+
+# 1. меняем staging:stage --> staging:live в консуле
+# 2. удаляем предыдущий live из консула
+# 3. стопаем предыдущий live в фарафоне (через 3 минуты)
+serve release --env 'live' --build-number '34'
+
 # скрипт-wrapper для регистрации
 serve consul supervisor \
   --name 'forgame-api3' \
@@ -9,6 +23,9 @@ serve consul supervisor \
   --staging 'live' \
   --port 12073
   bin/start
+
+
+# вариант без использования manifest.yml
 
 serve marathon deploy-site \
   --marathon 'mesos1-q.qa.inn.ru' \
@@ -35,20 +52,3 @@ serve marathon deploy-task \
   --mem 512 \
   --constraints 'cpb' \
   --envs '{"ENV":"qa"}'
-
-
-# вариант с manifest.yaml
-
-# для serve на Go машинках нужен будет /etc/serve/config.yml, 
-# в котором будут адреса марафона, реестра пакетов и прочие конфиги
-
-# 1. запускаем новую версию, дожидаемся появления в консуле
-# 2. находим предыдущую версию и убираем ее сначала из консула, потом из марафона
-# todo: надо подумать как находить полную версию только по build-number
-# --branch 'master' — опционально поле, 'master' по-умолчанию
-serve deploy --env 'qa' --branch 'master' --build-number '34'
-
-# 1. меняем staging:stage --> staging:live в консуле
-# 2. удаляем предыдущий live из консула
-# 3. стопаем предыдущий live в фарафоне (через 3 минуты)
-serve release --env 'live' --build-number '34'
